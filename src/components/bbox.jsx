339b57d8e0c7df69e7d4e50feee450fdb946e766
joal84 <joalmonog@gmail.com>
2023-02-28 11:59:21 +0100

    label element added

--- src/components/bbox.jsx
+++ src/components/bbox.jsx
@@ -15,38 +15,58 @@ export default function Bbox({ fetchOSM, setCoord, setUrl, coord }) {
     <div className="coord-container">
       <Title text="Geolocation Box" />
       <form onSubmit={handleSubmit} className="form-container">
-        <input
-          type="text"
-          name="min-longitude"
-          placeholder="Min-Longitude"
-          value={coord.minLongitude}
-          onChange={(e) => setCoord({ ...coord, minLongitude: e.target.value })}
-          className="input"
-        />
-        <input
-          type="text"
-          name="min-latitude"
-          placeholder="Min-Latitude"
-          value={coord.minLatitude}
-          onChange={(e) => setCoord({ ...coord, minLatitude: e.target.value })}
-          className="input"
-        />
-        <input
-          type="text"
-          name="max-longitude"
-          placeholder="Max-Longitude"
-          value={coord.maxLongitude}
-          onChange={(e) => setCoord({ ...coord, maxLongitude: e.target.value })}
-          className="input"
-        />
-        <input
-          type="text"
-          name="max-latitude"
-          placeholder="Max-Latitude"
-          value={coord.maxLatitude}
-          onChange={(e) => setCoord({ ...coord, maxLatitude: e.target.value })}
-          className="input"
-        />
+        <div className="field-label">
+          <label htmlFor="min-longitude">Min-Longitude</label>
+          <input
+            type="text"
+            name="min-longitude"
+            placeholder="13.38640"
+            value={coord.minLongitude}
+            onChange={(e) =>
+              setCoord({ ...coord, minLongitude: e.target.value })
+            }
+            className="input"
+          />
+        </div>
+        <div className="field-label">
+          <label htmlFor="min-latitude">Min-Latitude</label>
+          <input
+            type="text"
+            name="min-latitude"
+            placeholder="52.51044"
+            value={coord.minLatitude}
+            onChange={(e) =>
+              setCoord({ ...coord, minLatitude: e.target.value })
+            }
+            className="input"
+          />
+        </div>
+        <div className="field-label">
+          <label htmlFor="max-longitude">Max-Longitude</label>
+          <input
+            type="text"
+            name="max-longitude"
+            placeholder="13.40325"
+            value={coord.maxLongitude}
+            onChange={(e) =>
+              setCoord({ ...coord, maxLongitude: e.target.value })
+            }
+            className="input"
+          />
+        </div>
+        <div className="field-label">
+          <label htmlFor="max-latitude">Max-Latitude</label>
+          <input
+            type="text"
+            name="max-latitude"
+            placeholder="52.51604"
+            value={coord.maxLatitude}
+            onChange={(e) =>
+              setCoord({ ...coord, maxLatitude: e.target.value })
+            }
+            className="input"
+          />
+        </div>
         {validation(coord) ? (
           <button onClick={handleSubmit} className="subbmit-btn">
             Submit
